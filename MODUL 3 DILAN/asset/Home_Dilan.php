f0232c7fbd70c838ff51676ca04ad4bf9506c2d7
syaukanied <syaukanie.dilan@gmail.com>
2022-11-20 20:47:35 +0700

    git push

---
+++ MODUL 3 DILAN/asset/Home_Dilan.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg fixed-top scroll-down bg-primary" style="color:rgb(41, 41, 41); ;">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a style="color: white" class="nav-link"></a>
+              </li>
+                <li class="nav-item">
+                    <a style="color: white" class="nav-link active" aria-current="page" href="Home_Dilan.php">Home</a>
+                  </li>
+              <li class="nav-item">
+                <a style="color: white" class="nav-link" href="Add_Dilan.php">My car</a>
+              </li>
+              <div class="d-flex">
+          <a href="./pages/add-hamka.php">
+          <button class="btn " type="submit" style="color: white;">Add car</button></a>
+      </div>
+        </div>
+      </nav>
+
+      <div>
+        <br>
+        <br>
+            <div style="margin: 40px;" class="row">
+              <div class="col">
+                <h1>Selamat Datang Di Show Room Dilan</h1>
+                <a>At lacus vitae nulla sagittis scelerisque nisl. Pellentesque duis cursus vestibulum, facilisi ac, sed faucibus.<a>
+                  <br>
+
+                  <a href="ListCar_Dilan.php">
+                     <button type="button" class="btn btn-primary btn-mycar mt-4">My Car</button>
+                   </a>
+
+                   <div class="w-100 mt-5"></div>
+                   <img src="logo-ead.png" alt="logo" width=20%>    
+                   <span class="ms-3">Dilan_1202204136</span>     
+                </div>
+              <div style="text-align :center" class="col">
+                <img src="download.jpg" alt="mobil" width=110% >
+            </div>
+          </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
