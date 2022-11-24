a205558e49815c41fcf948486e3c4868415254a5
alfianmf <alfianm999@gmail.com>
2022-11-24 14:08:28 +0700

    debug

--- MODUL 3 DILAN/asset/config/delete.php
+++ MODUL 3 DILAN/asset/config/delete.php
@@ -1,8 +1,8 @@
-<?php
-    require './connector.php';
+        <?php
+            require './connector.php';
 
-    $id_mobil = $_GET['id_mobil'];
+            $id_mobil = $_GET['id_mobil'];
 
-    mysqli_query($connect, "DELETE FROM showroom_dilan_table WHERE id_mobil = $id_mobil");
-    header("location: ../ListCar_Dilan.php");
-?>
+            mysqli_query($connect, " FROM showroom_dilan_table WHERE id_mobil = $id_mobil");
+            header("location: ../ListCar_Dilan.php");
+        ?>
