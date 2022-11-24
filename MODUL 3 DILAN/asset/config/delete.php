b402e8b26a87b45d1dc1f22b4e7a790cf761e1d7
syaukanied <syaukanie.dilan@gmail.com>
2022-11-24 14:26:48 +0700

    Perbaikan bug

--- MODUL 3 DILAN/asset/config/delete.php
+++ MODUL 3 DILAN/asset/config/delete.php
@@ -3,6 +3,6 @@
 
             $id_mobil = $_GET['id_mobil'];
 
-            mysqli_query($connect, " FROM showroom_dilan_table WHERE id_mobil = $id_mobil");
+            mysqli_query($connect, "DELETE FROM showroom_dilan_table WHERE id_mobil = $id_mobil");
             header("location: ../ListCar_Dilan.php");
         ?>
